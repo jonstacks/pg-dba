39fb290170e0a504720ee31320bd1040361d6ca0
Jonathan Stacks <jonstacks13@gmail.com>
2021-07-16 21:42:52 -0500

    Add additional tests for config

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -15,6 +15,51 @@ func withEnvSet(key, value string, f func(key string)) {
 	os.Unsetenv(key)
 }
 
+func testAllBoolVariable(t *testing.T, key string, defaultValue bool, f func() bool) {
+	for _, v := range []string{"true", "TRUE", "TrUE"} {
+		withEnvSet(key, v, func(k string) {
+			assert.True(t, f())
+		})
+	}
+
+	for _, v := range []string{"false", "FALSE", "FaLSE"} {
+		withEnvSet(key, v, func(k string) {
+			assert.False(t, f())
+		})
+	}
+
+	assert.Equal(t, defaultValue, f())
+}
+
+func testAllStringVariable(t *testing.T, key string, defaultValue string, f func() string) {
+	assert.Equal(t, defaultValue, f())
+
+	for _, v := range []string{"value one", "value_two", "VALUE_THREE"} {
+		withEnvSet(key, v, func(k string) {
+			assert.Equal(t, v, f())
+		})
+	}
+}
+
+func testAllTimeoutVariable(t *testing.T, key string, defaultValue int, f func() (int, error)) {
+	val, err := f()
+	assert.NoError(t, err)
+	assert.Equal(t, defaultValue, val)
+
+	for intVal, stringVal := range map[int]string{100: "100", 200: "200", 1000: "1000"} {
+		withEnvSet(key, stringVal, func(k string) {
+			val, err := f()
+			assert.NoError(t, err)
+			assert.Equal(t, intVal, val)
+		})
+	}
+
+	withEnvSet(key, "SHOULD NOT PARSE", func(k string) {
+		_, err := f()
+		assert.Error(t, err)
+	})
+}
+
 func TestEnvBool(t *testing.T) {
 	withEnvSet("BOOLEAN_KEY", "wrong", func(key string) {
 		val := envBool(key, true)
@@ -92,4 +137,54 @@ func TestLogLevel(t *testing.T) {
 	})
 
 	assert.Equal(t, logrus.InfoLevel, LogLevel())
+}
+
+func TestAnalyzeTimeoutSeconds(t *testing.T) {
+	testAllTimeoutVariable(t, "ANALYZE_TIMEOUT_SECONDS", 600, AnalyzeTimeoutSeconds)
+}
+
+func TestFullVacuumTimeoutSeconds(t *testing.T) {
+	testAllTimeoutVariable(t, "FULL_VACUUM_TIMEOUT_SECONDS", 600, FullVacuumTimeoutSeconds)
+}
+
+func TestVacuumTimeoutSeconds(t *testing.T) {
+	testAllTimeoutVariable(t, "VACUUM_TIMEOUT_SECONDS", 600, VacuumTimeoutSeconds)
+}
+
+func TestBloatQueryTimeoutSeconds(t *testing.T) {
+	testAllTimeoutVariable(t, "BLOAT_QUERY_TIMEOUT_SECONDS", 30, BloatQueryTimeoutSeconds)
+}
+
+func TestPostAnalyze(t *testing.T) {
+	testAllBoolVariable(t, "POST_ANALYZE", true, PostAnalyze)
+}
+
+func TestPreAnalyze(t *testing.T) {
+	testAllBoolVariable(t, "PRE_ANALYZE", true, PreAnalyze)
+}
+
+func TestVerbose(t *testing.T) {
+	testAllBoolVariable(t, "VERBOSE", false, Verbose)
+}
+
+func TestDBHost(t *testing.T) {
+	testAllStringVariable(t, "POSTGRES_HOST", "localhost", DBHost)
+}
+
+func TestDBUser(t *testing.T) {
+	testAllStringVariable(t, "POSTGRES_USER", "postgres", DBUser)
+}
+
+func TestDBName(t *testing.T) {
+	testAllStringVariable(t, "POSTGRES_DB", "postgres", DBName)
+}
+
+func TestDBPassword(t *testing.T) {
+	testAllStringVariable(t, "POSTGRES_PASSWORD", `""`, DBPassword)
+}
+
+func TestDBConnectionString(t *testing.T) {
+	withEnvSet("SSL_MODE", "require", func(key string) {
+		assert.Equal(t, `host=localhost dbname=postgres user=postgres password="" sslmode=require`, DBConnectionString())
+	})
 }
